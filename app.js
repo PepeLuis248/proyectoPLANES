6796ca81d9ee1b304c21c96aca420cfc068fb501
Jose Luis Planes <jlplanes@uolsinectis.com.ar>
2025-03-08 11:41:47 -0300

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log('Ejecutando el sistema?)
-=======
-console.log("Ejecutando en sistema")
->>>>>>> 0c4ffdc62ecf0c5e49d291893c0bfff144c59fd5
+console.log('Ejecutando el sistema')
